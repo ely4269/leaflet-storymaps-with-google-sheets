073576cf8c733c86c43495c762d8a1c784ace417
ely4269 <168016464+ely4269@users.noreply.github.com>
2024-05-08 19:17:15 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://ely4269.github.io/leaflet-storymaps-with-google-sheets.github.io/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ynsUomMIFlYHswDIyDv9UwMg-_7NQRc2Ng6mqohTjdk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
